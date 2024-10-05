f8a086fcee73fd8a69a1467973d139f7f196fcae
Ali PARLATTI <aliparlatti@gmail.com>
2024-10-05 10:40:48 +0300

    feature: add supabase

---
+++ src/app/shared/services/supabase.service.ts
@@ -0,0 +1,26 @@
+
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
+  protected async getTableData(tableName: string) {
+    const { data, error } = await this.supabase
+      .from(tableName)
+      .select('*');
+    if (error) {
+      throw new Error(error.message);
+    }
+    return data;
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://gximeeiraenbblncdzip.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4aW1lZWlyYWVuYmJsbmNkemlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5NTIwNzAsImV4cCI6MjA0MzUyODA3MH0.vL0JvrPJy7kPwST9OPGxtm4hwmdNfykHBzUbz_WbKb8',
+};
