37611ec0cb04193664c3aa52b2941adc1525a81e
Ali PARLATTI <aliparlatti@gmail.com>
2024-10-08 00:11:12 +0300

    feature: add splashScreen images

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -15,7 +15,23 @@ const config: CapacitorConfig = {
       "androidClientId": "227419213250-97fun5umc2sp4oeobg0n0pa24rfrc7uk.apps.googleusercontent.com",
       "clientId": "227419213250-97fun5umc2sp4oeobg0n0pa24rfrc7uk.apps.googleusercontent.com",
       "forceCodeForRefreshToken": true
-    }
+    },
+    SplashScreen: {
+      launchShowDuration: 1000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#ff430d",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
   }
 };
 
